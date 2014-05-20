84e68f7d807a573341e354227e0023328f6f3ced
Pierre <pflauder@gmail.com>
2014-05-20 12:11:27 +0200

    First commit

---
+++ master.js
@@ -0,0 +1,56 @@
+{
+	"iuid": "3e2d0d40-05e8-4e23-8c71-f0f62c1f49fe",
+	"name" "Tequila"
+},
+{
+	"iuid": "9705e725-b777-41b6-b957-ce762dec8e3d",
+	"name" "Brown rum"
+},
+{
+	"iuid": "b5df5bc6-1b26-4702-bcbe-73a2709c4e23",
+	"name" "White rsum"
+},
+{
+	"iuid": "fc00dd7e-f9e8-47c9-8183-f9cdc2f93dac",
+	"name" "Pastis"
+},
+{
+	"iuid": "055c749e-ce5c-468f-af04-ddb89a7cb286",
+	"name" "Gin"
+},
+{
+	"iuid": "6b5f1ced-8c42-4508-b9e2-61df6652367c",
+	"name" "Vodka"
+},
+{
+	"iuid": "c52124ed-7be0-4636-ac45-12c906cab0eb",
+	"name" "Whisky"
+},
+{
+	"iuid": "a627427e-e6a2-414b-8881-0cb2835a71ea",
+	"name" "Malibu coco"
+},
+{
+	"iuid": "79e42999-7b7e-42e8-b162-f6f199f55363",
+	"name" "Cointreau"
+},
+{
+	"iuid": "8550ee0b-aa35-45b7-ad12-1bdc6e08d67c",
+	"name" "Blue Curacao"
+},
+{
+	"iuid": "1484bc7f-5f81-4f30-b675-e3e22d43ed56",
+	"name" "Triple Sec"
+},
+{
+	"iuid": "9146eaeb-32fd-4810-8954-9f396958bdde",
+	"name" "Orange juice"
+},
+{
+	"iuid": "4d1bfc3c-ff17-4e22-b240-fc604761c893",
+	"name" "Cranberry juice"
+},
+{
+	"iuid": "53d9525f-61ac-4990-a045-aa4f17503ce7",
+	"name" "Grenadine syrup"
+}
---
+++ version.js
@@ -0,0 +1,4 @@
+{
+	"version": 0.1,
+	"checksum": "a202bd4a37b5d24fc3eb571921bb416070331eda"
+}
